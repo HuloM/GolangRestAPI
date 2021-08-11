16146fe937659571425b44ed6c34ca10af4f0fdd
matt <matthieu.l.dubray@gmail.com>
2021-08-10 19:30:35 -0700

    new dependancy

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
-	gorm.io/gorm v1.21.12 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.12
 )
--- internal/database/database.go
+++ internal/database/database.go
@@ -2,11 +2,31 @@ package database
 
 import (
 	"fmt"
-
 	"gorm.io/gorm"
+
+	"os"
 )
 
 func NewDatabase() (*gorm.DB, error) {
-	gorm.Model
+	fmt.Println("Setting up new database connection")
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_TABLE")
+	dbPort := os.Getenv("DB_PORT")
+
+	connectStr := fmt.Sprintf("host=%s port=%s user =%s dbname=%s password%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+
+	db, err := gorm.Open("postgres", connectStr)
+	if err != nil {
+		return db, nil
+	}
+
+	if err := db.DB().Ping(); err != nil {
+		return db, err
+	}
+
+
 	return nil, nil
 }
